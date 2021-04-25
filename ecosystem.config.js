9d8e2f448d36455306f6fd64546346549335d379
Aigars Bedeicis <a.bedeicis@gmail.com>
2021-04-25 04:51:28 +0300

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'prod',
-      script: 'yarn prod:start',
-      watch: '.',
+      script: 'serve',
+      env: {
+        PM2_SERVE_PATH: './build/',
+        PM2_SERVE_PORT: 5000,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: './index.html',
+      },
     },
   ],
 };
