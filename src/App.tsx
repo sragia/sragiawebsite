45ecdee18bbf404a97f35210e708d1d7bc6b688c
Aigars Bedeicis <a.bedeicis@gmail.com>
2023-09-22 02:16:47 +0300

    Improve transition between light colors

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ const interval = 2000;
 export default function App() {
   const { color, color2, deg, oldColors, activeState } = useRandomColorShift({ interval });
   return (
-    <div className="flex h-[100vh] flex-col items-center bg-black transition-all">
+    <div className="flex h-[100vh] flex-col items-center transition-all" style={{
+      background: color || '#000'
+    }}>
       <div
         className={classnames(
           'absolute left-0 top-0 h-full w-full transition-opacity duration-1000',
